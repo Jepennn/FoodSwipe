42486c521b54dbf724d338138da1cf099bb88bf3
Jesper Sandberg Hesselgren <jespersh@kth.se>
2025-05-03 21:08:40 +0200

    Added tailwind and removed all unnecessary css files

--- src/View/cardView.jsx
+++ src/View/cardView.jsx
@@ -1,4 +1,3 @@
-import styles from "./cardView.module.css";
 import { useSelector } from "react-redux";
 
 export function CardView({ foodImage, foodTitle }) {
@@ -8,8 +7,14 @@ export function CardView({ foodImage, foodTitle }) {
     <img src="/spinner.svg"></img>
   ) : (
     <>
-      <img className={styles.dish_image} src={foodImage} alt="image of food" />
-      <h1 className={styles.dish_title}>{foodTitle}</h1>
+      <img
+        className="w-[85%] h-[85%] object-cover rounded-[1rem]"
+        src={foodImage}
+        alt="image of food"
+      />
+      <h1 className="text-2xl text-center my-6 text-[var(--text-color)]">
+        {foodTitle}
+      </h1>
     </>
   );
 }
--- src/View/sidebarViewRightButtonTop.jsx
+++ src/View/sidebarViewRightButtonTop.jsx
@@ -1,9 +1,8 @@
 import { FiSidebar } from "react-icons/fi";
-import styles from "./sidebarView.module.css";
 
 export function SidebarViewRightButtonTop({ onToggleSidebar }) {
   return (
-    <div className={styles.right_button_top}>
+    <div className="absolute top-4 right-4 p-2 text-gray-600 hover:text-gray-900 cursor-pointer">
       <FiSidebar onClick={onToggleSidebar} size={25} />
     </div>
   );
